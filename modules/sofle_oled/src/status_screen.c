72b277bfe0391d6444071b57e87ae69b68d3ca8d
adixonjcdev <adixonjc.dev@gmail.com>
2026-09-15 01:59:21 -0500

    Invert custom OLED monochrome colors

--- modules/sofle_oled/src/status_screen.c
+++ modules/sofle_oled/src/status_screen.c
@@ -4,15 +4,15 @@
 lv_obj_t *zmk_display_status_screen(void) {
     lv_obj_t *screen = lv_obj_create(NULL);
 
-    /*
-     * OLED monocromático:
-     * fondo apagado y sin bordes/padding.
-     */
     lv_obj_remove_style_all(screen);
 
+    /*
+     * En este OLED monocromático la polaridad efectiva
+     * está invertida respecto a lo que esperábamos.
+     */
     lv_obj_set_style_bg_color(
         screen,
-        lv_color_black(),
+        lv_color_white(),
         LV_PART_MAIN
     );
 
@@ -22,16 +22,13 @@ lv_obj_t *zmk_display_status_screen(void) {
         LV_PART_MAIN
     );
 
-    /*
-     * Texto SOFLE.
-     */
     lv_obj_t *label = lv_label_create(screen);
 
     lv_obj_remove_style_all(label);
 
     lv_obj_set_style_text_color(
         label,
-        lv_color_white(),
+        lv_color_black(),
         LV_PART_MAIN
     );
 
