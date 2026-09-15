bed4401c680d486c6d4050cc213e0b814bd0140a
adixonjcdev <adixonjc.dev@gmail.com>
2026-09-14 21:23:05 -0500

    Add custom Sofle OLED screen

---
+++ modules/sofle_oled/src/status_screen.c
@@ -0,0 +1,13 @@
+#include <zephyr/kernel.h>
+#include <lvgl.h>
+
+lv_obj_t *zmk_display_status_screen() {
+    lv_obj_t *screen = lv_obj_create(NULL);
+
+    lv_obj_t *label = lv_label_create(screen);
+    lv_label_set_text(label, "SOFLE");
+
+    lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
+
+    return screen;
+}
