a479a7085ed64406feac0653abce5679de3a0b7c
adixonjcdev <adixonjc.dev@gmail.com>
2026-09-15 15:38:56 -0500

    Add ADX logo to right OLED

--- modules/sofle_oled/src/status_screen_right.c
+++ modules/sofle_oled/src/status_screen_right.c
@@ -1,6 +1,8 @@
 #include <zephyr/kernel.h>
 #include <lvgl.h>
 
+#include "logo.h"
+
 /*
  * Pantalla personalizada del OLED derecho.
  */
@@ -10,8 +12,8 @@ lv_obj_t *zmk_display_status_screen(void) {
     /*
      * SSD1306:
      *
-     * WHITE lógico = píxel apagado
-     * BLACK lógico = píxel encendido
+     * WHITE lógico = píxel apagado / fondo negro físico
+     * BLACK lógico = píxel encendido / blanco físico
      */
     lv_obj_remove_style_all(screen);
 
@@ -28,36 +30,55 @@ lv_obj_t *zmk_display_status_screen(void) {
     );
 
     /*
-     * Texto de prueba.
+     * Creamos el logo píxel por píxel.
+     *
+     * El bitmap generado contiene:
+     * 0xFF = píxel perteneciente al logo
+     * 0x00 = píxel vacío
      */
-    lv_obj_t *title = lv_label_create(screen);
+    for (int y = 0; y < ADX_LOGO_HEIGHT; y++) {
+        for (int x = 0; x < ADX_LOGO_WIDTH; x++) {
 
-    lv_obj_remove_style_all(title);
+            int index = (y * ADX_LOGO_WIDTH) + x;
 
-    lv_obj_set_style_text_color(
-        title,
-        lv_color_black(),
-        LV_PART_MAIN
-    );
+            if (adx_logo_map[index] == 0xFF) {
 
-    lv_obj_set_style_text_opa(
-        title,
-        LV_OPA_COVER,
-        LV_PART_MAIN
-    );
+                lv_obj_t *pixel = lv_obj_create(screen);
 
-    lv_obj_set_style_text_font(
-        title,
-        &lv_font_montserrat_14,
-        LV_PART_MAIN
-    );
+                lv_obj_remove_style_all(pixel);
 
-    lv_label_set_text(
-        title,
-        "SOFLE"
-    );
+                lv_obj_set_size(
+                    pixel,
+                    1,
+                    1
+                );
 
-    lv_obj_center(title);
+                lv_obj_set_style_bg_color(
+                    pixel,
+                    lv_color_black(),
+                    LV_PART_MAIN
+                );
+
+                lv_obj_set_style_bg_opa(
+                    pixel,
+                    LV_OPA_COVER,
+                    LV_PART_MAIN
+                );
+
+                /*
+                 * Centrar logo de 45x30 dentro de 128x32.
+                 *
+                 * X = (128 - 45) / 2 = 41
+                 * Y = (32 - 30) / 2 = 1
+                 */
+                lv_obj_set_pos(
+                    pixel,
+                    41 + x,
+                    1 + y
+                );
+            }
+        }
+    }
 
     return screen;
 }
