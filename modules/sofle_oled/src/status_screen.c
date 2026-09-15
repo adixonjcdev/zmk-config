965ad3d26d9af8862dc4e4dbd5864e8dc51cdca8
adixonjcdev <adixonjc.dev@gmail.com>
2026-09-15 11:44:56 -0500

    Align OLED title and layer on top row test y and x

--- modules/sofle_oled/src/status_screen.c
+++ modules/sofle_oled/src/status_screen.c
@@ -121,7 +121,7 @@ lv_obj_t *zmk_display_status_screen(void) {
 
     lv_obj_set_style_text_font(
         layer_label,
-        &lv_font_montserrat_10,
+        &lv_font_montserrat_14,
         LV_PART_MAIN
     );
 
@@ -131,7 +131,7 @@ lv_obj_t *zmk_display_status_screen(void) {
         layer_label,
         LV_ALIGN_TOP_RIGHT,
         2,
-        0
+        -1
     );
 
     return screen;
